a687388fef9665a83a906d2cb8a42c895693e6ae
AbdElrahman Sha3rawy <abdoooeid124@gmail.com>
2025-12-23 02:42:24 +0200

    .0.

---
+++ Sign Up/script.js
@@ -0,0 +1,5 @@
+let link=document.querySelector('.link');
+let Create_Account=document.querySelector('.Create_Account');
+        Create_Account.style.opacity = "0.5";
+        link.style.pointerEvents = "none"; 
+        link.style.curser = "pointer"; 
